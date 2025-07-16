4d1cd7bdf0ea91ae2c20df8ede5d8c28160fc2f0
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2025-07-16 17:44:58 +0530

    Add validation for quantity and price

--- src/services/addSweet.js
+++ src/services/addSweet.js
@@ -2,12 +2,20 @@ const Sweet = require('../models/Sweet');
 
 
 const addSweet = async (sweetData) => {
-    const {name} = sweetData;
+    const {name,price,quantity} = sweetData;
     try{
 
-       if (!name || typeof name !== 'string' || name.trim() === '') {
-    throw new Error('Sweet name is required and must be a non-empty string.');
-  }
+    if (!name || typeof name !== 'string' || name.trim() === '') {
+      throw new Error('Sweet name is required and must be a non-empty string.');
+     }
+
+     if (typeof price !== 'number' || price <= 0) {
+       throw new Error('Price must be a positive number.');
+     }
+
+     if (!Number.isInteger(quantity) || quantity < 0) {
+       throw new Error('Quantity must be a non-negative integer.');
+     }
         const sweet = new Sweet(sweetData);
         const savedSweet = await sweet.save();
         return savedSweet;
