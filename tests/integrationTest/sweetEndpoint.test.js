a74373170af25b01bbb39d45a2fde3bed1e84648
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2025-07-16 21:37:11 +0530

    Update sweetEndpoint test file Added  integration test  for view all sweet endpoint

--- tests/integrationTest/sweetEndpoint.test.js
+++ tests/integrationTest/sweetEndpoint.test.js
@@ -34,4 +34,11 @@ describe('integration tests for sweet endpoints', () => {
     sweetId = res.body._id;
   });
 
+  test("GET /api/sweets - View all sweets", async () => {
+    const res = await request(app).get("/api/sweets");
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+
 })
