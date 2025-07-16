33d786e562a41e5f55acfdc8f2689c70969261f3
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2025-07-16 17:39:50 +0530

    name validation logic added and test passed successfully in addSweet services

--- src/services/addSweet.js
+++ src/services/addSweet.js
@@ -2,7 +2,12 @@ const Sweet = require('../models/Sweet');
 
 
 const addSweet = async (sweetData) => {
+    const {name} = sweetData;
     try{
+
+       if (!name || typeof name !== 'string' || name.trim() === '') {
+    throw new Error('Sweet name is required and must be a non-empty string.');
+  }
         const sweet = new Sweet(sweetData);
         const savedSweet = await sweet.save();
         return savedSweet;
